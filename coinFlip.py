ab87d5839651a2bf89a39bcefcc23cced15a11ac
Nicholas Kohut <nicholaskohut@nicholass-air-2.lan>
2023-02-09 22:35:07 -0800

    Fixing a typo

--- coinFlip.py
+++ coinFlip.py
@@ -2,8 +2,8 @@
 import numpy as np
 
 def coinFlip():
-    Coin = np.random.rand(1)
-    if Coin < 0.5:
+    coin = np.random.rand(1)
+    if coin < 0.5:
         return "H"
     else:
         return "T"
@@ -11,7 +11,7 @@ def coinFlip():
 def countChange(n):
     change = 0
     last_flip = coinFlip()
-    for i in range(1, n):
+    for i in range(1, n-1):
         new_flip = coinFlip()
         if new_flip != last_flip:
             change = change + 1
